c89c31000893b25341e24dc10603abad5833a9cc
Filip Oščádal <filip@mxd.cz>
2010-10-19 17:52:57 +0200

    scripts for easy backups

---
+++ backup-root.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Distributed under the terms of the GNU General Public License v3
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY. YOU USE AT YOUR OWN RISK. THE AUTHOR
+# WILL NOT BE LIABLE FOR DATA LOSS, DAMAGES, LOSS OF PROFITS OR ANY
+# OTHER  KIND OF LOSS WHILE USING OR MISUSING THIS SOFTWARE.
+# See the GNU General Public License for more details.
+
+# change to match your backup media location!
+BACKUP='/media/PET'
+
+cd /
+sudo tar -cvpzf "$BACKUP/root-backup-`date +%d.%m.%Y`.tar.gz" --one-file-system --exclude=/proc --exclude=/media --exclude=/lost+found --exclude=/sys --exclude=/mnt --exclude=/media --exclude=/dev / 2>/dev/null
