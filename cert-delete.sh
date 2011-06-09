85c15fe111884811d8e518e93fc3c1baf4b39752
peep <peep@peep.(none)>
2011-06-09 13:33:09 +0200

    certificate management

---
+++ cert-delete.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
+#
+# Filip Oscadal <filip@mxd.cz> http://mxd.cz No Rights Reserved 2011.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY. YOU USE AT YOUR OWN RISK. THE AUTHOR
+# WILL NOT BE LIABLE FOR DATA LOSS, DAMAGES, LOSS OF PROFITS OR ANY
+# OTHER  KIND OF LOSS WHILE USING OR MISUSING THIS SOFTWARE.
+# See the GNU General Public License for more details.
+
+certutil -D -n $1 -d sql:$HOME/.pki/nssdb
+
+exit 0
---
+++ cert-import.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
+#
+# Filip Oscadal <filip@mxd.cz> http://mxd.cz No Rights Reserved 2011.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY. YOU USE AT YOUR OWN RISK. THE AUTHOR
+# WILL NOT BE LIABLE FOR DATA LOSS, DAMAGES, LOSS OF PROFITS OR ANY
+# OTHER  KIND OF LOSS WHILE USING OR MISUSING THIS SOFTWARE.
+# See the GNU General Public License for more details.
+
+# check syntax
+if [ -z "$1" ]
+then
+  echo "\nSyntax: $(basename $0) <remotehost> [<port>]\n"
+  exit 1
+fi
+
+REMHOST=$1
+REMPORT=${2:-443}
+
+exec 6>&1
+exec > $REMHOST
+echo | openssl s_client -connect ${REMHOST}:${REMPORT} 2>&1 |sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'
+certutil -d sql:$HOME/.pki/nssdb -A -t TC -n "$REMHOST" -i $REMHOST 
+exec 1>&6 6>&-
+
+echo "Done."
+exit 0
