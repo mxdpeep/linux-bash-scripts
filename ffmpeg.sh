096a279ee7179b2930412bbe5afd96b1e8d41249
Fred Brooker <git@gscloud.cz>
2025-07-15 08:25:42 +0200

    minor update

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
 
-DOCKER_IMAGE="lscr.io/linuxserver/ffmpeg:latest"
-HOST_CWD="$(pwd)"
-CONTAINER_MEDIA_DIR="/config"
-
-docker run \
-  --rm \
-  -it \
-  -v "${HOST_CWD}:${CONTAINER_MEDIA_DIR}" \
-  "${DOCKER_IMAGE}" \
-  ffmpeg "$@"
-
-# docker run --rm -it \
-#  -v $(pwd):/config \
-#  linuxserver/ffmpeg \
-#  -i /config/input.mkv \
-#  -c:v libx264 \
-#  -b:v 4M \
-#  -vf scale=1280:720 \
-#  -c:a copy \
-#  /config/output.mkv
+set -x
+docker run --rm -v "$(pwd):/config" -w /config lscr.io/linuxserver/ffmpeg:latest "$@"
